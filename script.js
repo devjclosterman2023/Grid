6702fb4639b1adf78b438bc86a83e6d13192e3ac
Jason C <Jason@web-mo.com>
2023-11-28 12:27:46 -0700

    HelloPortfolio

--- script.js
+++ script.js
@@ -1,25 +1,3 @@
-console.log("I am working for you Mr.Closterman");
-
-// document.addEventListener('DOMContentLoaded', function () {
-//   const cards = document.querySelectorAll('.profile-card');
-  
-//   cards.forEach(card => {
-//       card.addEventListener('mouseenter', () => {
-//           cards.forEach(otherCard => {
-//               if (otherCard !== card) {
-//                   otherCard.classList.add('unhovered');
-//               }
-//           });
-//       });
-//       card.addEventListener('mouseleave', () => {
-//           cards.forEach(otherCard => {
-//               if (otherCard !== card) {
-//                   otherCard.classList.remove('unhovered');
-//               }
-//           });
-//       });
-//   });
-// });
 'use strict';
 
 
